798e47f5bc7525792518ac3feb9f345f47675db8
Jodi Bell <jodi@jodibell.com>
2019-03-04 17:46:12 -0500

    teacher page ready for db integration

--- public/js/page-scripts/table-custom-elements.js
+++ public/js/page-scripts/table-custom-elements.js
@@ -1,21 +1,30 @@
 (function ($) {
   $(document).ready(function(){
-
+    //This is all you need to update to pull in student data
     var data =
-      [["1","Tiger Nixon","System Architect",'<span class="new badge" data-badge-caption="">Edinburgh</span>',"5421","2011/04/25","$320,800"],
-       ["2","Garrett Winters","Accountant",'<span class="new badge green" data-badge-caption="">Tokyo</span>',"8422","2011/07/25","$170,750"],
-       ["3","Ashton Cox","Junior Technical Author",'<span class="new badge blue" data-badge-caption="">San Francisco</span>',"1562","2009/01/12","$86,000"],
-       ["4","Cedric Kelly","Senior Javascript Developer",'<span class="new badge" data-badge-caption="">Edinburgh</span>',"6224","2012/03/29","$433,060"],
-       ["5","Airi Satou","Accountant",'<span class="new badge green" data-badge-caption="">Tokyo</span>',"5407","2008/11/28","$162,700"],
-       ["6","Brielle Williamson","Integration Specialist",'<span class="new badge orange" data-badge-caption="">New York</span>',"4804","2012/12/02","$372,000"],
-       ["7","Herrod Chandler","Sales Assistant",'<span class="new badge blue" data-badge-caption="">San Francisco</span>',"9608","2012/08/06","$137,500"],
-       ["8","Rhona Davidson","Integration Specialist",'<span class="new badge green" data-badge-caption="">Tokyo</span>',"6200","2010/10/14","$327,900"],
-       ["9","Colleen Hurst","Javascript Developer",'<span class="new badge blue" data-badge-caption="">San Francisco</span>',"2360","2009/09/15","$205,500"],
-       ["10","Sonya Frost","Software Engineer",'<span class="new badge" data-badge-caption="">Edinburgh</span>',"1667","2008/12/13","$103,600"],
-       ["11","Jena Gaines","Office Manager",'<span class="new badge red" data-badge-caption="">London</span>',"3814","2008/12/19","$90,560"],
-       ["12","Quinn Flynn","Support Lead",'<span class="new badge" data-badge-caption="">Edinburgh</span>',"9497","2013/03/03","$342,000"],
-       ["13","Charde Marshall","Regional Director",'<span class="new badge blue" data-badge-caption="">San Francisco</span>',"6741","2008/10/16","$470,600"],
-       ["14","Haley Kennedy","Senior Marketing Designer",'<span class="new badge red" data-badge-caption="">London</span>',"3597","2012/12/18","$313,500"]];
+      [["1","Tiger Nixon","raise hand before speaking"], 
+      ["2","Tiger Nixon","raise hand before speaking"],
+      ["3","Tiger Nixon","raise hand before speaking"]
+    ];
+
+    //This handles the buttons. The console log is logging the events on click and showing the correct value, so you should be able to use this and modify to capture each unique id with value.
+    data.forEach((e, i) => {
+      return (
+        e.push(
+          `<form><label><input class="with-gap" value="1" name="group-${i}" type="radio"/>
+      <span>Met</span> </label>
+      <label><input class="with-gap" value="0" name="group-${i}" type="radio"/>
+      <span>Not Met</span></label>
+      <label> <input class="with-gap" value="null" name="group-${i}" type="radio"/>
+      <span>N/A</span></label></form>`
+        )
+      )
+    });
+
+    $(".container").on("click", ".with-gap", function(event) {
+      console.log(event);
+      //replace with update function for updating behaviors
+    });
 
     var tableCustomElements = $('#table-custom-elements');
     if (tableCustomElements.length) {
@@ -26,10 +35,10 @@
            'searchable':false,
            'orderable':false,
            'className': 'dataTables-checkbox-column',
-           'render': function (data, type, full, meta){
+           /*'render': function (data, type, full, meta){
                return '<label><input class="filled-in" type="checkbox" name="id[]" value="'
                   + $('<div/>').text(data).html() + '"><span></span></label>';
-           }
+           }*/
         }],
         'language': {
           'search': '',
