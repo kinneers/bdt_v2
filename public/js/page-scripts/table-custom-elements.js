67b2e39e9926984b57994be10da1919ad7760170
Jodi Bell <jodi@jodibell.com>
2019-03-06 16:53:34 -0500

    adding button and file styling

--- public/js/page-scripts/table-custom-elements.js
+++ public/js/page-scripts/table-custom-elements.js
@@ -1,27 +1,39 @@
 (function ($) {
-  $(document).ready(function(){
+  $(document).ready(function () {
     //This is all you need to update to pull in student data
     var data =
-      [["1","Tiger Nixon","raise hand before speaking"], 
-      ["2","Tiger Nixon","raise hand before speaking"],
-      ["3","Tiger Nixon","raise hand before speaking"]
-    ];
+      [["1", "Tiger Nixon", "raise hand before speaking"],
+      ["2", "Tiger Nixon", "raise hand before speaking"],
+      ["3", "Tiger Nixon", "raise hand before speaking"]
+      ];
 
     //This handles the buttons. The console log is logging the events on click and showing the correct value, so you should be able to use this and modify to capture each unique id with value.
     data.forEach((e, i) => {
       return (
         e.push(
-          `<form><label><input class="with-gap" value="1" name="group-${i}" type="radio"/>
+          `<form id="student-${i}"><label><input class="with-gap" id="met-${i}" name="group-${i}" type="radio"/>
       <span>Met</span> </label>
-      <label><input class="with-gap" value="0" name="group-${i}" type="radio"/>
-      <span>Not Met</span></label>`
+      <label><input class="with-gap" id="notMet-${i}" name="group-${i}" type="radio"/>
+      <span>Not Met</span></label></form>`
         )
       )
     });
 
-    $(".container").on("click", ".with-gap", function(event) {
-      console.log(event);
-      //replace with update function for updating behaviors
+    $(".container").on("click", "#saveData", function (event) {
+      for (var i = 0; i < data.length; i++) {
+        var met = $(`#met-${i}:checked`).val();
+        //var met = $(`#met-${data[i].id}:checked`).val();
+        var notMet = $(`#notMet-${i}:checked`).val();
+        console.log(i);
+        console.log(met);
+        console.log(notMet);
+        if (met === "on") {
+          //ajax call or something to post to db
+          //post to student[i] or student.id
+          //${i} will change to data[i].id
+        }
+        //else return 0 instead
+      }
     });
 
     var tableCustomElements = $('#table-custom-elements');
@@ -29,14 +41,14 @@
       var table = tableCustomElements.DataTable({
         'data': data,
         'columnDefs': [{
-           'targets': 0,
-           'searchable':false,
-           'orderable':false,
-           'className': 'dataTables-checkbox-column',
-           /*'render': function (data, type, full, meta){
-               return '<label><input class="filled-in" type="checkbox" name="id[]" value="'
-                  + $('<div/>').text(data).html() + '"><span></span></label>';
-           }*/
+          'targets': 0,
+          'searchable': false,
+          'orderable': false,
+          'className': 'dataTables-checkbox-column',
+          /*'render': function (data, type, full, meta){
+              return '<label><input class="filled-in" type="checkbox" name="id[]" value="'
+                 + $('<div/>').text(data).html() + '"><span></span></label>';
+          }*/
         }],
         'language': {
           'search': '',
@@ -47,7 +59,7 @@
         'scrollY': '400px',
         'scrollCollapse': true,
         'pagingType': 'full',
-        'drawCallback': function( settings ) {
+        'drawCallback': function (settings) {
           var api = this.api();
 
           // Add waves to pagination buttons
@@ -57,13 +69,13 @@
         }
       });
 
-      $('#table-custom-elements_wrapper').on('change', 'input[type=checkbox]', function(e) {
+      $('#table-custom-elements_wrapper').on('change', 'input[type=checkbox]', function (e) {
         var parentTR = $(this).parentsUntil('table').closest('tr');
         parentTR.toggleClass('selected', this.checked);
       });
 
       // Handle click on "Select all" control
-      $('#table-custom-elements_wrapper').find('.select-all').on('click', function(){
+      $('#table-custom-elements_wrapper').find('.select-all').on('click', function () {
         // Check/uncheck all checkboxes in the table
         var rows = table.rows({ 'search': 'applied' }).nodes();
         $('input[type="checkbox"]', rows)
@@ -74,4 +86,4 @@
 
 
   });
-}( jQuery ));
+}(jQuery));
