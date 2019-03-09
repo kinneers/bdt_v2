372a72ce9cc9eea364d3979ba0966b5c8ca9ea61
Sarah Kinneer <kinneers@gmail.com>
2019-03-09 04:53:00 -0500

    changed to production db

--- routes/teacher-routes.js
+++ routes/teacher-routes.js
@@ -7,7 +7,7 @@ module.exports = function (app, passport) {
     //Route to get the student names and behaviors belonging to current staff user for display in rating table
     app.get("/teacher/students", isLoggedIn, function (req, res) {
         var currentUser = req.user.username;
-        models.sequelize.query(`SELECT CONCAT(S_TS.firstName, ' ', S_TS.lastName) AS studentname, B.behavior, B.id FROM ( SELECT S.userName, S.firstName, S.lastName FROM behavior_db.students AS S INNER JOIN behavior_db.teacherstudents AS TS ON S.userName = TS.StudentUserName WHERE TS.TeacherUserName = '${currentUser}') AS S_TS INNER JOIN behavior_db.behaviors AS B ON B.StudentUserName = S_TS.userName;`).spread((dbData, metadata) => {
+        models.sequelize.query(`SELECT CONCAT(S_TS.firstName, ' ', S_TS.lastName) AS studentname, B.behavior, B.id FROM ( SELECT S.userName, S.firstName, S.lastName FROM ifqqmoj8gh2zitgi.students AS S INNER JOIN ifqqmoj8gh2zitgi.teacherstudents AS TS ON S.userName = TS.StudentUserName WHERE TS.TeacherUserName = '${currentUser}') AS S_TS INNER JOIN ifqqmoj8gh2zitgi.behaviors AS B ON B.StudentUserName = S_TS.userName;`).spread((dbData, metadata) => {
             res.json(dbData);
         })
     });
